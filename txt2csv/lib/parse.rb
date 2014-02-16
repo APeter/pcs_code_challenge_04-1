4fca9cbce98f6f72b1010b2bbba53312e7aaef4a
Kate Flood <katefloodster@gmail.com>
2014-02-16 15:25:31 -0800

    rubocop commit

--- txt2csv/lib/parse.rb
+++ txt2csv/lib/parse.rb
@@ -1,3 +1,4 @@
+# This class parses!
 class Parse
   def self.parse_names(prefixes, suffixes, name_string)
     parsed_name = { pre: '', first: '', middle: '', last: '', suffix: '' }
@@ -13,12 +14,11 @@ class Parse
   end
 
   def self.parse_phone(phone_string)
-
     parsed_phone = { country: '', area: '', prefix: '', line: '', extension: '' }
 
     phone_string.gsub!(/\W/, ' ')
     phone = phone_string.split
-    parsed_phone[:extension] = phone.pop.sub(/x/, "") if phone_string.match(/x/)
+    parsed_phone[:extension] = phone.pop.sub(/x/, '') if phone_string.match(/x/)
     parsed_phone[:line] = phone.pop
     parsed_phone[:prefix] = phone.pop
     parsed_phone[:area] = phone.pop
@@ -30,12 +30,11 @@ class Parse
   def self.parse_twitter(twit_string)
     twitter = []
 
-    twitter << twit_string.gsub(/@/, "")
+    twitter << twit_string.gsub(/@/, '')
   end
 
   def self.parse_email(email_string)
     email = []
-    email.push(email_string.match(/\S+@\S+\.\S+/) ? email_string : "not found")
+    email.push(email_string.match(/\S+@\S+\.\S+/) ? email_string : 'not found')
   end
 end
-
--- txt2csv/spec/analyze_spec.rb
+++ txt2csv/spec/analyze_spec.rb
@@ -5,44 +5,44 @@ require 'spec_helper'
 #   1) so that the test can be run on its own and
 #   2) so the contents of the file are clear
 
-def create_test_file (filename)
-  File.open(filename, "w") do |f|
-    5.times  {f.puts "Mr. Jones"}
-    6.times  {f.puts "Miss Smith"}
-    4.times  {f.puts "Mrs. Wesson"}
-    10.times {f.puts "Dr. Roberts"}
-    1.times  {f.puts "Jane Wintermute"}
-    2.times  {f.puts "Frank Franklin"}
-    3.times  {f.puts "Darleen Washington"}
- end
+def create_test_file(filename)
+  File.open(filename, 'w') do |f|
+    5.times  { f.puts 'Mr. Jones' }
+    6.times  { f.puts 'Miss Smith' }
+    4.times  { f.puts 'Mrs. Wesson' }
+    10.times { f.puts 'Dr. Roberts' }
+    1.times  { f.puts 'Jane Wintermute' }
+    2.times  { f.puts 'Frank Franklin' }
+    3.times  { f.puts 'Darleen Washington' }
+  end
 end
 
-def create_prefix_expected_file (filename)
+def create_prefix_expected_file(filename)
   # Note sort order - by count, not by word
-  File.open(filename, "w") do |f|
-    f.puts "Dr. 10"
-    f.puts "Miss 6"
-    f.puts "Mr. 5"
-    f.puts "Mrs. 4"
-    f.puts "Darleen 3"
-    f.puts "Frank 2"
-    f.puts "Jane 1"
+  File.open(filename, 'w') do |f|
+    f.puts 'Dr. 10'
+    f.puts 'Miss 6'
+    f.puts 'Mr. 5'
+    f.puts 'Mrs. 4'
+    f.puts 'Darleen 3'
+    f.puts 'Frank 2'
+    f.puts 'Jane 1'
   end
 end
 
-def create_suffix_expected_file (filename)
-  File.open(filename, "w") do |f|
-    f.puts "Roberts 10"
-    f.puts "Smith 6"
-    f.puts "Jones 5"
-    f.puts "Wesson 4"
-    f.puts "Washington 3"
-    f.puts "Franklin 2"
-    f.puts "Wintermute 1"
+def create_suffix_expected_file(filename)
+  File.open(filename, 'w') do |f|
+    f.puts 'Roberts 10'
+    f.puts 'Smith 6'
+    f.puts 'Jones 5'
+    f.puts 'Wesson 4'
+    f.puts 'Washington 3'
+    f.puts 'Franklin 2'
+    f.puts 'Wintermute 1'
   end
 end
 
-describe "analyze" do
+describe 'analyze' do
 
   # Set up the files need for the specifications
   # put them down in the spec folder so they don't clutter the project root folder
@@ -64,13 +64,13 @@ describe "analyze" do
 
   # specify what the options and STDIN and STDOUT are supposed to do
 
-  it "reads a file and prints a hash of prefixes when given the -p option" do
+  it 'reads a file and prints a hash of prefixes when given the -p option' do
 
     `ruby lib/analyze.rb -p <spec/testfile.txt >spec/histogram.txt`
     IO.read('spec/histogram.txt').should == IO.read('spec/expected_prefixes.txt')
   end
 
-  it "reads a file and prints a hash of suffixes when given the -s option" do
+  it 'reads a file and prints a hash of suffixes when given the -s option' do
     `ruby lib/analyze.rb -s <spec/testfile.txt >spec/histogram.txt`
     IO.read('spec/histogram.txt').should == IO.read('spec/expected_suffixes.txt')
   end
